ae5638939ec8274e810c93ba932bd4013cad2393
xuzk <xuzk@nrec.com>
2022-04-21 14:23:50 +0800

    预测脚本改为从数据库中读取

--- run.py
+++ run.py
@@ -1,23 +1,15 @@
 # coding=utf-8
 from Fund import Fund
+import pandas as pd
+from db_fund_io import engine
 
-fund_info = {
-    "002979": "广发金融地产",
-    "001549": "天弘上证50",
-    "005918": "天弘沪深300",
-    "001595": "天弘中证银行",
-    "001551": "天弘中证医药100",
-    "161725": "招商中证白酒",
-    "001593": "天弘创业板",
-    "001632": "天弘中证食品饮料",
-    "070039": "嘉实中证500",
-    "002903": "广发中证500",
-    "002987": "广发沪深300"
-}
 
 if __name__ == '__main__':
-    for k, v in fund_info.items():
+    df = pd.read_sql("code_name_map", engine)
+    for _, k, v, state, topic in df.itertuples():
+        if state not in ["持有", "定投"]:
+            continue
         print("\n", k, v)
-        fund = Fund(k, v, result_dir="持有")
+        fund = Fund(k, v, result_dir=state)
         fund.forecasting(step=90)
         fund.validate()
