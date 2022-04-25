a469e31c1c5396aec3f20028167372e9a587eef2
xuzk <xuzk@nrec.com>
2022-04-25 09:00:31 +0800

    根据当前时间是否是工作日，且基金净值是否更新，判断是否需要爬取今日净值

--- database_util.py
+++ database_util.py
@@ -4,6 +4,7 @@ from urllib.parse import quote_plus as urlquote
 
 import pandas as pd
 import sqlalchemy
+from chinese_calendar import is_workday
 from sqlalchemy import create_engine, select, func, MetaData, Table, Column, inspect
 
 from spider import get_records
@@ -14,6 +15,37 @@ address = "localhost"
 port = "3306"
 database = "fund"
 engine = create_engine(f'mysql+pymysql://{user}:{urlquote(password)}@{address}:{port}/{database}')
+
+
+def get_newest_value_date():
+    """
+    根据当前时间判断净值是否更新，若更新返回今天的日期，否则返回上个交易日的日期
+    :return:
+    :rtype:
+    """
+
+    def is_trade_day(d):
+        if is_workday(d) and n_time.isoweekday() < 6:
+            return True
+        return False
+
+    # 范围时间
+    d_time = datetime.datetime.strptime(str(datetime.datetime.now().date()) + '20:30', '%Y-%m-%d%H:%M')
+    # 当前时间
+    n_time = datetime.datetime.now()
+
+    if is_trade_day(n_time) and d_time < n_time:
+        print("今日基金净值已更新")
+        return n_time.date()
+    else:
+        print("今日未更新，或今日不是交易日，以上一个交易日为净值获取截止时间\n")
+        n_time += datetime.timedelta(days=-1)
+        while not is_trade_day(n_time):
+            n_time += datetime.timedelta(days=-1)
+        return n_time.date()
+
+
+newest_value_date = get_newest_value_date()
 
 
 class FundInfoTable:
@@ -58,8 +90,8 @@ class FundInfoTable:
         :return:
         :rtype:
         """
-        if self.nearest_date != date.today():
-            start = str(self.nearest_date+datetime.timedelta(1))
+        if self.nearest_date != newest_value_date:
+            start = str(self.nearest_date + datetime.timedelta(1))
             records = get_records(self.code, start=start, end=str(date.today()))
             df = pd.DataFrame(records)
             if df.shape[1] > 1:  # 当前日期为空时会插入只有code列的空值
@@ -76,7 +108,7 @@ class FundInfoTable:
     def run(self):
         if inspect(engine).has_table(self.code):
             self.nearest_date = self.get_nearest_date()
-            if self.nearest_date != date.today():
+            if self.nearest_date != newest_value_date:
                 records = get_records(self.code, start=str(self.nearest_date), end=str(date.today()))
                 df = pd.DataFrame(records)
                 df.to_sql(self.table_name, engine, if_exists="append", index=False, dtype=self.dtype)
